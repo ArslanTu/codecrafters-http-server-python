090380e8e7cecb3f2a9bdab4415a1eeb3e603bac
ArslanTu <arslantu@arslantu.xyz>
2024-04-13 10:58:50 +0800

    test

--- app/main.py
+++ app/main.py
@@ -19,10 +19,17 @@ def main():
     req_lines = req.split("\r\n")
     req_start_line = req_lines[0]
     req_path = req_start_line.split(" ")[1]
-    if req_path == "/":
-        client_socket.send(b"HTTP/1.1 200 OK\r\n\r\n")
-    else:
-        client_socket.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
+    # if req_path == "/":
+    #     client_socket.send(b"HTTP/1.1 200 OK\r\n\r\n")
+    # else:
+    #     client_socket.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
+
+    # stage 4
+    res_body = req_path[len("/echo/"):]
+    client_socket.send(
+        b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\n"
+        + res_body.encode()
+    )
 
 
 if __name__ == "__main__":
